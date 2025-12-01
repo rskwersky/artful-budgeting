9d87b2a205b58cc140c8cd774b2b03bae26965b1
rskwersky <rskwersky2025@berkeley.edu>
2025-12-01 12:00:36 -0500

    restructure

---
+++ processing/cleaning.py
@@ -0,0 +1,15 @@
+import re
+
+def clean_merchant(name: str) -> str:
+    name = name.lower()
+    name = re.sub(r"\d+", "", name)
+    name = re.sub(r"s\s#", "", name)
+    name = re.sub(r"#", "", name)
+    
+    for city in ["san francisco", "berkeley", "ca", "oakland"]:
+        name = name.replace(city, "")
+    
+    for n in ["pos", "transaction", "counter", "purchase", "terminal", "card"]:
+        name = name.replace(n, "")
+
+    return name.strip()
